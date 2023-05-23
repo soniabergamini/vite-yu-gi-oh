95d5a63ae31c9e9493d7b0c8dbb25966ff1ead58
soniabergamini <sounia96@outlook.it>
2023-05-23 21:59:31 +0200

    perf(main): remove static src url

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,7 +23,7 @@ export default {
 
         <!-- Title -->
         <div class="text-white bg-secondaryBlack p-3">
-            <!-- <pre>{{ store.cardData[0] }}</pre> -->
+            <pre>{{ store.cardData[0] }}</pre>
             <h2 class="font-bold text-lg md:text-xl">Found {{ store.cardData[0].length }} cards</h2>
         </div>
 
@@ -31,8 +31,7 @@ export default {
         <section class="flex-wrap gap-[0.5em]">
             <div v-for="item in store.cardData[0]"
                 class="w-[calc(100%/2-1em)] md:w-[calc(100%/4-1em)] lg:w-[calc(100%/5-1em)] bg-primaryYellow mb-1">
-                <img :src="getImgPath(item.card_images[0].image_url_small)"
-                    src="https://images.ygoprodeck.com/images/cards_small/34541863.jpg" alt="card-img" class="w-full">
+                <img :src="getImgPath(item.card_images[0].image_url_small)" alt="card-img" class="w-full">
                 <div class="text-center p-3">
                     <h3 class="font-bold text-white mb-5 text-xs md:text-base">{{ item.name.toUpperCase() }}
                     </h3>
