61b9330dcd5c3d92c084ee31378bd5106d63d8f8
soniabergamini <sounia96@outlook.it>
2023-05-24 19:52:04 +0200

    feat(main): add update cards function

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -1,5 +1,6 @@
 <script>
 import { store } from '../data/store';
+import axios from 'axios';
 
 export default {
     name: "AppSelect",
@@ -9,9 +10,15 @@ export default {
             chosenCard: "All"
         }
     },
-    methods: {
+    computed: {
+        // Return cards from archetypes filter
         updateCards() {
-            console.log("Chosen card: ", this.chosenCard)
+            console.log("Chosen card: ", this.chosenCard.trim());
+            let urlAPI = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=' + this.chosenCard;
+            axios.get(urlAPI).then(response => {
+                this.store.cardData = [];
+                this.store.cardData.push(response.data.data);
+            })
         }
     }
 }
@@ -22,7 +29,7 @@ export default {
     <!-- Select -->
     <section class="p-5">
         <!-- <pre>{{ store.archetypesData[0] }}</pre> -->
-        <select class="py-1.5 px-1 w-1/6 border rounded" v-model="chosenCard" @change="updateCards()">
+        <select class="py-1.5 px-1 w-1/6 border rounded" v-model="chosenCard" @change="updateCards">
             <option selected>All</option>
             <option v-for="item in store.archetypesData[0]">{{ item.archetype_name }}</option>
         </select>
